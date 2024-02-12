c3b3bb655f3466185c3b2f4b99e3bf4a2661e7c0
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-02-12 15:08:00 -0500

    Student class created

---
+++ src/org/university/Class.java
@@ -0,0 +1,12 @@
+package org.university;
+
+import java.util.List;
+
+public class Class {
+    private String name;
+    private String code;
+    private String classroom;
+    private List<Student> students;
+    private List<Teacher> teachers;
+}
+
