61a3dac3abd9dca4e04b353d6cda9a1c17f6b210
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-02-12 16:57:47 -0500

    university initialization

---
+++ src/org/university/Menu.java
@@ -0,0 +1,16 @@
+package org.university;
+
+import com.sun.xml.internal.ws.wsdl.writer.document.Part;
+
+public class Menu {
+    private University university;
+
+    public Menu() {
+        this.university = new University("My University");
+    }
+
+
+    public void startMenu() {
+
+    }
+}
