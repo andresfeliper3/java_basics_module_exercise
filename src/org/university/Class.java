51888e23a478364ed5c9ade27bb846d08967bdd8
andresfeliper3 <andresfeliper.2001@gmail.com>
2024-02-12 16:08:02 -0500

    Class implementation

--- src/org/university/Class.java
+++ src/org/university/Class.java
@@ -1,5 +1,6 @@
 package org.university;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Class {
@@ -7,6 +8,48 @@ public class Class {
     private String code;
     private String classroom;
     private List<Student> students;
-    private List<Teacher> teachers;
+    private Teacher teacher;
+
+    public Class(String name, String code) {
+        this.name = name;
+        this.code = code;
+        this.students = new ArrayList<>();
+    }
+
+    public void assignClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public void assignTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public void addStudents(List<Student> students) {
+        this.students.addAll(students);
+    }
+
+    public void addStudent(Student student) {
+        this.students.add(student);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
 }
 
